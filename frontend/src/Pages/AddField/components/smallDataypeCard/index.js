36ab6c89bd9746e25e5cf3b8b207d498b706e0d1
Charles <charlesmbrady@gmail.com>
2020-06-29 13:13:16 -0400

    added yellow highlight around selected datatype on /newField page

--- frontend/src/Pages/AddField/components/smallDataypeCard/index.js
+++ frontend/src/Pages/AddField/components/smallDataypeCard/index.js
@@ -9,8 +9,20 @@ export default function SmallDatatypeCard({ datatype }) {
     setField({ ...field, datatype: datatype });
   };
   return (
-    <div className='smallDatatypeCard' onClick={() => handleUpdate()}>
-      <h4 className='darkHeader'>{datatype}</h4>
+    <div
+      // if field.datatype == this datatype, add a "selected" class which will give highlighted border
+      className='smallDatatypeCard'
+      onClick={() => handleUpdate()}
+    >
+      <h4
+        className={
+          field.datatype == datatype
+            ? 'darkHeader selectedDatatype'
+            : 'darkHeader'
+        }
+      >
+        {datatype}
+      </h4>
     </div>
   );
 }
